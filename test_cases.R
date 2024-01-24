2624c91ab714bb487c0b8eb99d9e5b51ff548b8a
Mark <Derry>
2024-01-24 00:25:23 -0700

    updating test cases

--- test_cases.R
+++ test_cases.R
@@ -1,8 +1,8 @@
 source("smith_analysis.R")
 # test cases
-employment_income = c(50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000)
+employment_income = c(50000, 100000, 150000, 200000, 250000, 300000, 350000)
 home_value = c(500000, 1000000, 1500000, 2000000, 2500000, 3000000)
-mortgage_balance = c(100000, 200000, 300000, 400000, 500000, 1000000, 1500000, 2000000, 2500000)
+mortgage_balance = c(100000, 200000, 300000, 400000, 500000, 1000000, 1500000, 2000000)
 loc_interest_rate = c(0.03, 0.04, 0.05, 0.06, 0.07, 0.08)
 dividend_yeild = c(0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12)
 dividend_type_split = c(0, 0.25, 0.5, 0.75, 1)
@@ -12,7 +12,7 @@ rm(employment_income, home_value, loc_interest_rate, dividend_yeild, mortgage_ba
 
 
 # check that the test cases are correct
-test_cases[home_value < mortgage_balance, mortgage_balance := home_value * 0.9]
+test_cases <- test_cases[home_value > mortgage_balance, ]
 test_cases[, loc_balance := 0.65 * (home_value - mortgage_balance)]
 test_cases[, dividend_income := loc_balance * dividend_yeild]
 test_cases[, interest_expense := loc_interest_rate * loc_balance]
@@ -30,8 +30,8 @@ test
 library(tictoc)
 tic()
 output <- data.table()
-# for (i in 1:nrow(test_cases)) {
-for (i in 1:1000) {
+for (i in 1:nrow(test_cases)) {
+# for (i in 1:1000) {
   test_case_i = calculate_tax_liability_sm(employment_income = test_cases$employment_income[i], 
                                            home_value = test_cases$home_value[i],
                                            mortgage_balance = test_cases$mortgage_balance[i],
@@ -55,14 +55,14 @@ out <- cbind(test_cases, output)
 out[, which(duplicated(names(out))) := NULL]
 # plot results 
 library(ggplot2)
-plot <- out[employment_income == 200000 & 
+plot <- out[employment_income == 350000 & 
                  home_value == 1000000 &
-                 loc_interest_rate == 0.06 &
-                 dividend_yeild == 0.06 & 
+                 dividend_yeild == 0.11 & 
                  dividend_type_split == 0.25, ]
 
 plot
-# plot 1
-ggplot(plot, aes(x = mortgage_balance, y = sm_effective_value)) + 
-  geom_point() +
-  geom_line()
+# plot 1 group by loc_interest_rate
+
+ggplot(plot, aes(x = loc_balance, y = sm_effective_value, group = loc_interest_rate)) + 
+  geom_point(aes(color=loc_interest_rate)) +
+  geom_line(aes(color=loc_interest_rate)) 
